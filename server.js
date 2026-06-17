55ebb0f73a4e050ed0a5de0ae02d118645857a83
khayckk <k.fronza2008@gmail.com>
2026-06-17 08:53:59 -0300

    att

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const porta = 8080;
 
 // Importamos o Controller
-const { listarCardapio } = require('./src/controllers/CardapioController');
+const { listarCardapio, buscaPorId } = require('./src/controllers/cardapioController');
 
 // Tradutores de JSON nativos do Express
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +12,8 @@ app.use(express.json());
 
 // A NOSSA VÁLVULA GET:
 app.get('/cardapio', listarCardapio);
+
+app.get ('/cardapio/:id', buscaPorId)
 
 app.listen(porta, () => {
     console.log(`[SUBMUNDO] Motor rodando perfeitamente na porta ${porta}...`);
--- src/controllers/cardapioController.js
+++ src/controllers/cardapioController.js
@@ -1,21 +1,32 @@
 // src/controllers/CardapioController.js
-const { cardapioOficial } = require('../models/CardapioModel');
+const { cardapioOficial } = require("../models/CardapioModel");
 
 const listarCardapio = (req, res) => {
-    console.log("[LOG] Alguém solicitou o cardápio oficial no servidor.");
+  console.log("[LOG] Alguém solicitou o cardápio oficial no servidor.");
 
-    // Devolvemos o Status 200 (OK) e o pacote JSON com os dados
-    return res.status(200).json({
-        sucesso: true,
-        quantidade_itens: cardapioOficial.length,
-        dados: cardapioOficial
-    });
+  // Devolvemos o Status 200 (OK) e o pacote JSON com os dados
+  return res.status(200).json({
+    sucesso: true,
+    quantidade_itens: cardapioOficial.length,
+    dados: cardapioOficial,
+  });
 };
 
-const buscaPorId = (req,res) => {
-    const idEncontrado = parseInt(req.params.id)
-    const itemEncontrado =  cardapioOficial.find((item) => item.id === idEncontrado)
-}
+const buscaPorId = (req, res) => {
+  const idEncontrado = parseInt(req.params.id);
+  const itemEncontrado = cardapioOficial.find(
+    (item) => item.id === idEncontrado
+  );
+  return res.status(200).json({
+    mensagem: "Item encontrado:",
+    item: itemEncontrado,
+  });
 
+  if (!itemEncontrado) {
+    return res.status(400).json({
+      mensagem: "Item Inexistente",
+    });
+  }
+};
 
-module.exports = { listarCardapio };
+module.exports = { listarCardapio, buscaPorId };
